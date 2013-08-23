040bcd8155c1bc23218e73ae40903334b6db63e0
Lachlan Dowding <lachlan@permafro.st>
2013-08-23 16:25:12 +1000

    tundra.tn.document.type:get - get bizdoc type directly from the BizDocTypeStore object, rather than via call to wm.tn.doctype:view

--- code/source/tundra/tn/document/type.java
+++ code/source/tundra/tn/document/type.java
@@ -1,8 +1,8 @@
 package tundra.tn.document;
 
 // -----( IS Java Code Template v1.2
-// -----( CREATED: 2013-07-03 09:46:11 EST
-// -----( ON-HOST: 172.16.189.216
+// -----( CREATED: 2013-08-23 16:22:53.226
+// -----( ON-HOST: -
 
 import com.wm.data.*;
 import com.wm.util.Values;
@@ -55,16 +55,13 @@ public final class type
 	// --- <<IS-START-SHARED>> ---
 	// returns the Trading Networks document type given the ID
 	public static com.wm.app.tn.doc.BizDocType get(String id, String name) throws ServiceException {
-	  IData pipeline = IDataFactory.create();
-	  IDataCursor cursor = pipeline.getCursor();
-	  IDataUtil.put(cursor, "typeId", id);
-	  IDataUtil.put(cursor, "typeName", name);
-	  cursor.destroy();
-	
-	  pipeline = tundra.service.invoke("wm.tn.doctype:view", pipeline);
-	
-	  cursor = pipeline.getCursor();
-	  return (com.wm.app.tn.doc.BizDocType)IDataUtil.get(cursor, "type");
+	  com.wm.app.tn.doc.BizDocType type = null;
+	  if (id != null) {
+	    type = com.wm.app.tn.db.BizDocTypeStore.get(id, true, true);
+	  } else if (name != null) {
+	    type = com.wm.app.tn.db.BizDocTypeStore.getByName(name, true, true);
+	  }
+	  return type;
 	}
 	// --- <<IS-END-SHARED>> ---
 }
