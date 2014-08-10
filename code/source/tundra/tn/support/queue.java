95533f3d988bcc68e54ae66eec7327c61b67a02e
Lachlan Dowding <lachlan@permafro.st>
2014-08-11 09:42:20 +1000

    Fix bug in tundra.tn.support.queue:each where tasks were retried even if they had not previously failed

--- code/source/tundra/tn/support/queue.java
+++ code/source/tundra/tn/support/queue.java
@@ -1,8 +1,8 @@
 package tundra.tn.support;
 
 // -----( IS Java Code Template v1.2
-// -----( CREATED: 2014-08-10 17:19:18 EST
-// -----( ON-HOST: 172.16.189.141
+// -----( CREATED: 2014-08-11 09:40:27.757
+// -----( ON-HOST: -
 
 import com.wm.data.*;
 import com.wm.util.Values;
@@ -41,27 +41,27 @@ public final class queue
 		// [o] field:0:required queue
 		// [o] field:0:optional logMsg
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		  String queue = IDataUtil.getString(cursor, "queue");
 		  String service = IDataUtil.getString(cursor, "$service");
 		  IData scope = IDataUtil.getIData(cursor, "$pipeline");
 		  String sConcurrency = IDataUtil.getString(cursor, "$concurrency");
 		  String sOrdered = IDataUtil.getString(cursor, "$ordered?");
-		
+
 		  int concurrency = 1;
 		  if (sConcurrency != null) concurrency = Integer.parseInt(sConcurrency);
-		
+
 		  boolean ordered = false;
 		  if (sOrdered != null) ordered = Boolean.parseBoolean(sOrdered);
-		
+
 		  each(queue, service, scope == null? pipeline : scope, concurrency, 0, 0, ordered);
 		} finally {
 		  cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 	// --- <<IS-START-SHARED>> ---
@@ -69,7 +69,7 @@ public final class queue
 	protected final static com.wm.lang.ns.NSName EXECUTE_TASK_SERVICE = com.wm.lang.ns.NSName.create(EXECUTE_TASK_SERVICE_NAME);
 	protected final static String DELIVER_BATCH_SERVICE_NAME = "wm.tn.queuing:deliverBatch";
 	protected static final String DELIVERY_JOB_UPDATE_SQL_STATEMENT = "deliver.job.update";
-	
+
 	// dequeues each task on the given TN queue, and processes the task using the given service and input pipeline;
 	// if concurrency > 1, tasks will be processed by a thread pool whose size is equal to the desired concurrency,
 	// otherwise they will be processed on the current thread
@@ -77,10 +77,10 @@ public final class queue
 	  try {
 	    if (batchLimit <= 0) batchLimit = Integer.MAX_VALUE;
 	    if (retryLimit <= 0) retryLimit = Byte.MAX_VALUE;
-	
+
 	    com.wm.app.tn.delivery.DeliveryQueue queue = com.wm.app.tn.db.QueueOperations.selectByName(queueName);
 	    if (queue == null) throw new ServiceException("Queue '" + queueName + "' does not exist");
-	
+
 	    if (concurrency <= 1) {
 	      eachDirect(queue, EXECUTE_TASK_SERVICE, service, pipeline, batchLimit, retryLimit, ordered);
 	    } else {
@@ -92,13 +92,13 @@ public final class queue
 	    throw new ServiceException(ex.getClass().getName() + ": " + ex.getMessage());
 	  }
 	}
-	
+
 	// dequeues each task on the given TN queue, and processes the task using the given service and input pipeline
 	// on the current thread
 	protected static void eachDirect(com.wm.app.tn.delivery.DeliveryQueue queue, com.wm.lang.ns.NSName executeTaskService, String service, IData pipeline, int batchLimit, int retryLimit, boolean ordered) throws ServiceException {
 	  boolean invokedByTradingNetworks = invokedByTradingNetworks();
 	  int total = 0;
-	
+
 	  try {
 	    while(total < batchLimit) {
 	      if (!invokedByTradingNetworks || queue.isEnabled() || queue.isDraining()) {
@@ -108,8 +108,8 @@ public final class queue
 	        } else {
 	          IData output = com.wm.app.b2b.server.Service.doInvoke(executeTaskService, createTaskInputPipeline(task, service, pipeline, queue.getQueueName(), queue.getQueueType()));
 	          total++;
-	
-	          if (retry(task, retryLimit, ordered)) { 
+
+	          if (retry(task, retryLimit, ordered)) {
 	            break; // if the task needs to be retried and the queue is being processed in-order, exit so as not to retry the same task immediately
 	          }
 	        }
@@ -126,24 +126,24 @@ public final class queue
 	    }
 	  }
 	}
-	
+
 	// dequeues each task on the given TN queue, and processes the task using the given service and input pipeline;
 	// tasks will be processed by a thread pool whose size is equal to the desired concurrency
 	protected static void eachConcurrent(com.wm.app.tn.delivery.DeliveryQueue queue, com.wm.lang.ns.NSName executeTaskService, String service, IData pipeline, int concurrency, int batchLimit) throws ServiceException {
 	  boolean invokedByTradingNetworks = invokedByTradingNetworks();
-	
+
 	  // upper bound on number of tasks submitted to thread pool at any one time
 	  int backlog = concurrency * 2;
 	  int total = 0;
-	
+
 	  com.wm.app.b2b.server.Session session = com.wm.app.b2b.server.Service.getSession();
 	  com.wm.app.b2b.server.InvokeState state = com.wm.app.b2b.server.InvokeState.getCurrentState();
-	
+
 	  java.util.concurrent.ExecutorService executor = java.util.concurrent.Executors.newFixedThreadPool(concurrency, new ServerThreadFactory(queue.getQueueName(), state));
-	
+
 	  java.util.Queue<java.util.concurrent.Future<IData>> futures = new java.util.LinkedList<java.util.concurrent.Future<IData>>();
 	  java.util.List<Exception> exceptions = new java.util.ArrayList<Exception>();
-	
+
 	  try {
 	    while(total < batchLimit) {
 	      if (!invokedByTradingNetworks || queue.isEnabled() || queue.isDraining()) {
@@ -183,7 +183,7 @@ public final class queue
 	    awaitAll(futures);
 	  }
 	}
-	
+
 	// waits for all futures in the given queue to complete
 	protected static <T> java.util.List<T> awaitAll(java.util.Queue<java.util.concurrent.Future<T>> futures) {
 	  java.util.List<T> results = new java.util.ArrayList<T>(futures.size());
@@ -192,17 +192,17 @@ public final class queue
 	  }
 	  return results;
 	}
-	
+
 	// waits for the first/head future in the given queue to complete
 	protected static <T> T awaitOldest(java.util.Queue<java.util.concurrent.Future<T>> futures) {
 	  return await(futures.poll());
 	}
-	
+
 	// waits for the given future to complete
 	protected static <T> T await(java.util.concurrent.Future<T> future) {
 	  T output = null;
 	  try {
-	    output = future.get(); 
+	    output = future.get();
 	  } catch (java.util.concurrent.ExecutionException ex) {
 	    // ignore exceptions
 	  } catch(InterruptedException ex) {
@@ -210,7 +210,7 @@ public final class queue
 	  }
 	  return output;
 	}
-	
+
 	// returns a new pipeline for the executeTaskService
 	protected static IData createTaskInputPipeline(com.wm.app.tn.delivery.GuaranteedJob task, String service, IData pipeline, String queueName, String queueType) throws java.io.IOException {
 	  IData output = IDataFactory.create();
@@ -222,10 +222,10 @@ public final class queue
 	  IDataUtil.put(cursor, "queue", queueName);
 	  IDataUtil.put(cursor, "queue.type", queueType);
 	  cursor.destroy();
-	
+
 	  return output;
 	}
-	
+
 	// returns true if the invocation callstack includes the WmTN/wm.tn.queuing:deliverBatch service
 	protected static boolean invokedByTradingNetworks() {
 	  java.util.Iterator iterator = com.wm.app.b2b.server.InvokeState.getCurrentState().getCallStack().iterator();
@@ -236,47 +236,47 @@ public final class queue
 	  }
 	  return result;
 	}
-	
+
 	// wraps a call to an IS service with a standard java.util.concurrent.callable interface, so that it can
 	// be used by java.util.concurrent executors
 	public static class CallableService implements java.util.concurrent.Callable<IData> {
 	  protected com.wm.lang.ns.NSName service;
 	  protected IData input;
 	  protected com.wm.app.b2b.server.Session session;
-	
+
 	  public CallableService(String service, com.wm.app.b2b.server.Session session, IData input) {
 	    this(com.wm.lang.ns.NSName.create(service), session, input);
 	  }
-	
+
 	  public CallableService(com.wm.lang.ns.NSName service, com.wm.app.b2b.server.Session session, IData input) {
 	    this.service = service;
 	    this.input = input;
 	    this.session = session;
 	  }
-	
+
 	  public IData call() throws Exception {
 	    return com.wm.app.b2b.server.Service.doInvoke(service, session, input);
 	  }
-	} 
-	
+	}
+
 	// a thread factory which creates IS threads with the given invoke state
 	public static class ServerThreadFactory implements java.util.concurrent.ThreadFactory {
 	  String queue;
 	  protected com.wm.app.b2b.server.InvokeState state;
 	  protected long count = 1;
-	
+
 	  public ServerThreadFactory(String queue, com.wm.app.b2b.server.InvokeState state) {
 	    this.queue = queue;
 	    this.state = state;
 	  }
-	
+
 	  public Thread newThread(Runnable r) {
 	    com.wm.app.b2b.server.ServerThread thread = new com.wm.app.b2b.server.ServerThread(r);
 	    thread.setInvokeState(cloneInvokeStateWithStack());
 	    thread.setName("TundraTN/Queue '" + queue + "' Thread#" + String.format("%03d", count++));
 	    return thread;
 	  }
-	
+
 	  protected com.wm.app.b2b.server.InvokeState cloneInvokeStateWithStack() {
 	    com.wm.app.b2b.server.InvokeState outputState = (com.wm.app.b2b.server.InvokeState)state.clone();
 	    java.util.Stack stack = (java.util.Stack)state.getCallStack().clone();
@@ -287,25 +287,26 @@ public final class queue
 	    return outputState;
 	  }
 	}
-	
+
 	// re-enqueues the given job for delivery, unless it has reached its retry limit
 	protected static boolean retry(com.wm.app.tn.delivery.GuaranteedJob job, int retryLimit, boolean ordered) throws java.sql.SQLException, java.io.IOException, ServiceException {
 	  boolean retry = false;
-	
+
 	  if (ordered) {
 	    job = com.wm.app.tn.db.DeliveryStore.getAnyJob(job.getJobId(), true); // refetch job from DB
-	
+
 	    int jobLimit = job.getRetryLimit();
 	    int retries = job.getRetries();
-	
+	    String status = job.getStatus();
+
 	    boolean automaticRetry = jobLimit > 0;
-	
+
 	    if (jobLimit > 0) {
-	      retry = retries < jobLimit;
+	      retry = retries < jobLimit && status.equals("QUEUED");
 	    } else {
-	      retry = retries < retryLimit;
+	      retry = retries < retryLimit && status.equals("FAILED");
 	    }
-	
+
 	    if (retry) {
 	      if (!automaticRetry) {
 	        job.retryFailed();
@@ -314,28 +315,28 @@ public final class queue
 	      }
 	      job.setTimeUpdated(job.getTimeCreated()); // force this job to the front of the queue
 	      update(job);
-	
+
 	      com.wm.app.tn.doc.BizDocEnvelope bizdoc = job.getBizDocEnvelope();
 	      if (bizdoc != null) com.wm.app.tn.db.BizDocStore.changeStatus(bizdoc, "QUEUED", "REQUEUED");
 	    }
 	  }
-	
+
 	  return retry;
 	}
-	
+
 	// saves the given job to the Trading Networks database
 	protected static void update(com.wm.app.tn.delivery.GuaranteedJob job) throws java.sql.SQLException, java.io.IOException {
 	  java.sql.Connection connection = null;
 	  java.sql.PreparedStatement statement = null;
-	
+
 	  try {
 	    connection = com.wm.app.tn.db.Datastore.getConnection();
 	    statement = com.wm.app.tn.db.SQLStatements.prepareStatement(connection, DELIVERY_JOB_UPDATE_SQL_STATEMENT);
 	    statement.clearParameters();
-	
+
 	    // instead of setting TimeUpdated to now, set it to the time in the job object
 	    com.wm.app.tn.db.SQLWrappers.setTimestamp(statement, 1, new java.sql.Timestamp(job.getTimeUpdated()));
-	
+
 	    com.wm.app.tn.db.SQLWrappers.setChoppedString(statement, 2, job.getStatus(), "DeliveryJob.JobStatus");
 	    statement.setInt(3, job.getRetries());
 	    com.wm.app.tn.db.SQLWrappers.setChoppedString(statement, 4, job.getTransportStatus(), "DeliveryJob.TransportStatus");
@@ -347,7 +348,7 @@ public final class queue
 	    com.wm.app.tn.db.SQLWrappers.setChoppedString(statement, 10, job.getQueueName(), "DeliveryQueue.QueueName");
 	    com.wm.app.tn.db.SQLWrappers.setChoppedString(statement, 11, job.getInvokeAsUser(), "DeliveryJob.UserName");
 	    com.wm.app.tn.db.SQLWrappers.setCharString(statement, 12, job.getJobId());
-	
+
 	    statement.executeUpdate();
 	    connection.commit();
 	  } catch (java.sql.SQLException ex) {
@@ -355,7 +356,7 @@ public final class queue
 	    throw ex;
 	  } finally {
 	    com.wm.app.tn.db.SQLStatements.releaseStatement(statement);
-	    com.wm.app.tn.db.Datastore.releaseConnection(connection);     
+	    com.wm.app.tn.db.Datastore.releaseConnection(connection);
 	  }
 	}
 	// --- <<IS-END-SHARED>> ---
