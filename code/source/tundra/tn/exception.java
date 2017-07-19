ce5ee8ab591663b31bfd7f7d79875e474e2ab7b5
Lachlan Dowding <lachlan@permafro.st>
2017-07-19 12:03:29 +1000

    Change tundra.tn:receive, tundra.tn:deliver, and tundra.tn.exception:handle attached content part names to all be consistent using the following format:
        <service>_<content>_<datetime>_<ulid>
    
    Where:
        <service>  = fully-qualified service name with non-alphanumerics replaced
                     with "." characters
        <content>  = a description of the type of content attached
        <datetime> = the current datetime in the format "yyyy-MM-dd'T'HH.mm.ss.SSS"
        <ulid>     = a newly generated ULID to ensure part name uniqueness
    
    This change ensures the content part names are legal filenames,
    which then supports exporting the transaction content via
    My webMethods correctly.

--- code/source/tundra/tn/exception.java
+++ code/source/tundra/tn/exception.java
@@ -1,8 +1,8 @@
 package tundra.tn;
 
 // -----( IS Java Code Template v1.2
-// -----( CREATED: 2017-06-04 17:44:18 EST
-// -----( ON-HOST: 192.168.66.132
+// -----( CREATED: 2017-07-19T16:44:11.089
+// -----( ON-HOST: -
 
 import com.wm.data.*;
 import com.wm.util.Values;
@@ -21,11 +21,13 @@ import permafrost.tundra.content.Content;
 import permafrost.tundra.content.ContentAttached;
 import permafrost.tundra.data.IDataHelper;
 import permafrost.tundra.data.IDataMap;
+import permafrost.tundra.id.ULID;
 import permafrost.tundra.lang.BaseException;
 import permafrost.tundra.lang.BooleanHelper;
 import permafrost.tundra.lang.ExceptionHelper;
 import permafrost.tundra.lang.SecurityException;
 import permafrost.tundra.lang.UnrecoverableException;
+import permafrost.tundra.server.ServiceHelper;
 import permafrost.tundra.time.DateTimeHelper;
 import permafrost.tundra.tn.document.BizDocEnvelopeHelper;
 // --- <<IS-END-IMPORTS>> ---
@@ -56,41 +58,42 @@ public final class exception
 		// [i] - field:0:required InternalID
 		// [i] object:0:required $exception
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		    BizDocEnvelope bizdoc = BizDocEnvelopeHelper.normalize(IDataHelper.get(cursor, "$bizdoc", IData.class, true));
 		    Throwable exception = IDataHelper.get(cursor, "$exception", Throwable.class, true);
 		    boolean statusSilence = IDataHelper.getOrDefault(cursor, "$status.silence?", Boolean.class, false);
-		
-		    String messageClass = "Processing", 
-		           messageSummary = exception.getMessage(), 
-		           messageDetail = exception.toString(), 
+		    String service = IDataHelper.getOrDefault(cursor, "$exception.service", String.class, "tundra.tn.exception:handle");
+
+		    String messageClass = "Processing",
+		           messageSummary = exception.getMessage(),
+		           messageDetail = exception.toString(),
 		           userStatus = "ERROR";
-		    
+
 		    if (exception instanceof IDataCodable) {
 		        IDataMap exceptionDocument = IDataMap.of(((IDataCodable)exception).getIData());
 		        if (!BooleanHelper.parse((String)exceptionDocument.get("$exception.recoverable?"), true)) {
 		            messageClass = "Unrecoverable";
 		            userStatus = "ABORTED";
 		        }
-		
+
 		        IDataMap exceptionContent = IDataMap.of((IData)exceptionDocument.get("$exception.content"));
-		
+
 		        if (exceptionContent.size() > 0) {
 		            try {
 		                byte[] content = (byte[])exceptionContent.get("$content");
 		                String contentType = (String)exceptionContent.get("$content.type");
-		                
+
 		                if (content != null && content.length > 0) {
-		                    String partName = "tundra.tn.exception:handle/" + exception.getClass().getSimpleName() + "/" + DateTimeHelper.now(null);
-		
+		                    String partName = service.replaceAll("\\W", ".") + "_exception_" + DateTimeHelper.now("yyyy-MM-dd'T'HH.mm.ss.SSS") + "_" + ULID.generate();
+
 		                    IDataMap scope = new IDataMap();
 		                    scope.put("$bizdoc", bizdoc);
 		                    scope.put("$part", partName);
 		                    scope.put("$content", content);
 		                    scope.put("$content.type", contentType);
 		                    scope.put("$overwrite?", "true");
-		
+
 		                    Service.doInvoke(NSName.create("tundra.tn.document.content:add"), scope);
 		                }
 		            } catch(Exception ex) {
@@ -98,17 +101,17 @@ public final class exception
 		            }
 		        }
 		    }
-		
+
 		    BizDocEnvelopeHelper.log(bizdoc, "ERROR", messageClass, messageSummary, messageDetail);
 		    BizDocEnvelopeHelper.setStatus(bizdoc, null, userStatus, statusSilence);
-		
+
 		    ExceptionHelper.raise(exception);
 		} finally {
 		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 
@@ -120,20 +123,20 @@ public final class exception
 		// @subtype unknown
 		// @sigtype java 3.5
 		// [i] field:0:optional $message
-		// [i] field:0:optional $type {"security","strict","malformed","validation","duplicate","unsupported"}
+		// [i] field:0:optional $type {&quot;security&quot;,&quot;strict&quot;,&quot;malformed&quot;,&quot;validation&quot;,&quot;duplicate&quot;,&quot;unsupported&quot;}
 		IDataCursor cursor = pipeline.getCursor();
-		
+
 		try {
 		    String message = IDataHelper.get(cursor, "$message", String.class);
 		    String type = IDataHelper.get(cursor, "$type", String.class);
-		
+
 		    raise(message, type);
 		} finally {
 		    cursor.destroy();
 		}
 		// --- <<IS-END>> ---
 
-                
+
 	}
 
 	// --- <<IS-START-SHARED>> ---
@@ -147,9 +150,9 @@ public final class exception
 	public static void raise(String message, String type) throws ServiceException {
 	    if (message == null) message = "";
 	    if (type == null) type = "";
-	
+
 	    ServiceException ex = null;
-	
+
 	    if (type.equals("security")) {
 	        ex = new SecurityException(message);
 	    } else if (type.equals("strict")) {
@@ -165,7 +168,7 @@ public final class exception
 	    } else {
 	        ex = new BaseException(message);
 	    }
-	
+
 	    throw ex;
 	}
 	// --- <<IS-END-SHARED>> ---
