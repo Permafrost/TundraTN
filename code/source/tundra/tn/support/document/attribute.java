d503aea0479f0448e0e564f37f51d094ba7cbdd6
Lachlan Dowding <lachlan@permafrost.space>
2019-02-28 16:37:34 +1000

    Change tundra.tn.content:route to not include any $attributes specified that do not exist or are not active in Trading Networks on the routed document
    Also reduces contention on the DeliveryJob table of Trading Networks
    delivery queues by caching in memory which queues have queued tasks
    for 500ms and first checking this cache to determine whether to
    continue whenever a queue's scheduled task runs. This feature is
    intended to improve TN queue performance / reduce impact on the TN
    database when there are many queue's scheduled with short polling
    intervals such as every 1 second.

---
+++ code/source/tundra/tn/support/document/attribute.java
@@ -0,0 +1,53 @@
+package tundra.tn.support.document;
+
+// -----( IS Java Code Template v1.2
+// -----( CREATED: 2019-02-28 16:35:17 GMT+10:00
+// -----( ON-HOST: PCLBKJP4M2.internal.qr.com.au
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import permafrost.tundra.data.IDataHelper;
+import permafrost.tundra.tn.document.BizDocAttributeHelper;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class attribute
+
+{
+	// ---( internal utility methods )---
+
+	final static attribute _instance = new attribute();
+
+	static attribute _newInstance() { return new attribute(); }
+
+	static attribute _cast(Object o) { return (attribute)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void sanitize (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(sanitize)>> ---
+		// @subtype unknown
+		// @sigtype java 3.5
+		// [i] record:0:optional $attributes
+		// [o] record:0:optional $attributes
+		IDataCursor cursor = pipeline.getCursor();
+		
+		try {
+		    IData attributes = IDataHelper.get(cursor, "$attributes", IData.class);
+		    IDataHelper.put(cursor, "$attributes", BizDocAttributeHelper.sanitize(attributes), false);
+		} finally {
+		    cursor.destroy();
+		}
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
