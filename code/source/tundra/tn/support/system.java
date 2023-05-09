248c376a51d21ede41b15844d38cd821bb4cb77c
Lachlan Dowding <lachlan@permafrost.space>
2023-05-10 06:01:52 +1000

    Add `tundra.tn.support.system:export` for exporting all the document attribute, document type, profile, delivery queue, and routing rule components as a stable and therefore comparable JSON string

---
+++ code/source/tundra/tn/support/system.java
@@ -0,0 +1,50 @@
+package tundra.tn.support;
+
+// -----( IS Java Code Template v1.2
+// -----( CREATED: 2023-05-10 05:52:31 EST
+// -----( ON-HOST: -
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import permafrost.tundra.tn.util.SystemHelper;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class system
+
+{
+	// ---( internal utility methods )---
+
+	final static system _instance = new system();
+
+	static system _newInstance() { return new system(); }
+
+	static system _cast(Object o) { return (system)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void export (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(export)>> ---
+		// @subtype unknown
+		// @sigtype java 3.5
+		// [o] field:0:required $system.tn.export.json
+		IDataCursor cursor = pipeline.getCursor();
+		
+		try {
+		    IDataUtil.put(cursor, "$system.tn.export.json", SystemHelper.export());
+		} finally {
+		    cursor.destroy();
+		}
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
---
+++ ns/tundra/tn/support/system/export/java.frag
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<Values version="2.0">
+  <value name="name">export</value>
+  <value name="encodeutf8">true</value>
+  <value name="body">SURhdGFDdXJzb3IgY3Vyc29yID0gcGlwZWxpbmUuZ2V0Q3Vyc29yKCk7Cgp0cnkgewogICAgSURh
+dGFVdGlsLnB1dChjdXJzb3IsICIkc3lzdGVtLnRuLmV4cG9ydC5qc29uIiwgU3lzdGVtSGVscGVy
+LmV4cG9ydCgpKTsKfSBmaW5hbGx5IHsKICAgIGN1cnNvci5kZXN0cm95KCk7Cn0=</value>
+</Values>
